a9638a8dedc7a434e1f36a68e6e783c85747dbca
mattiasmak <mattias@tunity.se>
2018-10-01 11:07:29 +0200

    Cleaned up a bit

--- src/index.js
+++ src/index.js
@@ -1,22 +1,15 @@
 
 const config = require('exp-config')
-
 const resolvers = require('./graphql/resolvers')
 const typeDefs = require('./graphql/types')
-
-// console.log(dataHandler.getGameProviders())
-// console.log(dataHandler.getFilteredData(null, 'width'))
-// console.log(dataHandler.getFilteredData({ gameProviders: null, gameCollectionIds: ['cup-2'] }))
-
-// const paginatedData = dataHandler.getFilteredData({ gameProviders: null, gameCollectionIds: ['cup-2'] })
-
-// console.log(paginatedData.page(0), paginatedData.pages)
-
 const { ApolloServer } = require('apollo-server')
-
 const server = new ApolloServer({ typeDefs, resolvers })
 
-server.listen({ port: config.server.port, playground: true })
+server.listen(
+  {
+    port: config.server.port,
+    playground: true
+  })
   .then(({ url }) => {
-    console.log(`🚀 Server ready at ${url}`)
+    console.log(`🚀 Server started at ${url}`)
   })
